6becfcb3654939f603271e3e2b889edcafd1b10c
anamoyee <106310501+anamoyee@users.noreply.github.com>
2026-09-23 14:09:02 +0200

    add global instance of formatter for easy use in simple cases

--- src/nya_fmt/__init__.py
+++ src/nya_fmt/__init__.py
@@ -2,3 +2,4 @@ from .formatter import Formatter as Formatter
 from .formatter import Styles as Styles
 from .formatter import displayers as displayers
 from .formatter import providers as providers
+from .formatter._global_instance import fmt as fmt
